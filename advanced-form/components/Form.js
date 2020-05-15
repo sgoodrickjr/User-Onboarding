168bd001e6d78c39196868e5f8d7603cc002bd06
Stephen Goodrick <sgoodrickjr@gmail.com>
2020-05-15 14:19:37 -0600

    working on it

--- advanced-form/components/Form.js
+++ advanced-form/components/Form.js
@@ -1,7 +1,95 @@
 import React, { useState } from 'react'
+import * as Yup from "yup";
 
-let schema = yup.string();
-await schema.isValid('hello world');
+const formSchema = Yup.object().shape({
+    email: Yup
+        .string()
+        .email("Must be a valid email address.")
+        .required("Must include email address."),
+    password: Yup
+        .string()
+        .min(6, "Passwords must be at least 6 characters long.")
+        .required("Password is Required")
+    terms: Yup
+        .boolean()
+        .oneOf([true], "You must accept Terms and Conditions")
+});
+
+const [errors, setError] = useState({
+    email: "",
+    password: "",
+    terms: ""
+});
+
+useEffect(() => {
+    formSchema.isValid(formState).then(valid => {
+      setButtonDisabled(!valid);
+    });
+  }, [formState]);
+
+  const inputChange = e => {
+    e.persist();
+
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.value)
+      .then(valid => {
+        setErrors({
+          ...errors,
+          [e.target.name]: ""
+        });
+      })
+      .catch(err => {
+        setErrors({
+          ...errors,
+          [e.target.name]: err.errors[0]
+        });
+      });
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value
+    });
+  };
+
+//   function LoginForm() {
+//     return (
+//       <form>
+//         <label htmlFor="email">
+//         <input type="email" name="email" placeholder="Email" />
+//         />
+//               {errors.email.length > 0 ? (<p className="error">{errors.email}</p>) : null}
+//         <label htmlFor="password">
+//         <input type="password" name="password" placeholder="Password" />
+//         {errors.password.length > 6 ? (<p className="error">{errors.email}</p>) : null}
+//          <label htmlFor="terms">
+//           Do you agree to the terms and conditions?
+//         <input type="checkbox">
+//         <button>Submit!</button>
+//         />
+//       </form>
+//     );
+//   }
+
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    formSchema.isValid(formState).then(valid => {
+      // console.log(valid);
+      setButtonDisabled(!valid);
+    });
+  }, [formState]);
+
+const formSubmit = e => {
+    e.preventDefault();
+    console.log("submitted!");
+    axios
+      .post("https://reqres.in/api/users", formState)
+      .then(res => {
+        setPost(res.data);
+        console.log("success", res);
+      })
+      .catch(err => console.log(err.response));
+  };
 
 const [user, setUser] = useState({
     name = ""
@@ -22,6 +110,7 @@ return (
         <label>
             Username:
           <input
+                id="usernameInput"
                 type="text"
                 name="username"
                 value={user.username}
@@ -31,6 +120,7 @@ return (
         <label>
             Password:
           <input
+                id="passwordInput"
                 type="text"
                 name="password"
                 value={user.password}
@@ -38,9 +128,10 @@ return (
             />
         </label>
         <label>
-            Terms of Service:
+           Do you agree to the terms and conditions?
             <input
-                name="TermsOfService"
+                id="termsInput"
+                name="terms"
                 type="checkbox"
                 checked={true}
                 onChange={handleInputChange} />
